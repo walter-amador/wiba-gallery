faa2a9a167e0971e058055dcc83508a88859900a
WalterIran <amadorwalter3418@gmail.com>
2022-02-16 17:47:16 -0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center">
+      <h1 className="text-3xl font-bold underline">WIBA GALLERY</h1>
+    </div>
+  );
+}
+
+export default App;
